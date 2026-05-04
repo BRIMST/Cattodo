4c3e3f68757da89b8166370374f47a2c49acc617
BRIMST <131634474+BRIMST@users.noreply.github.com>
2026-05-04 17:28:46 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// SW desactivado - limpia caché
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
